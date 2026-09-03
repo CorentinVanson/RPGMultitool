d4c62cf329ee593322e1ed0961128479acd7ef72
Corentin Vanson <corentinvanson@hotmail.com>
2026-09-03 20:29:05 +0200

    update des visuels 4

--- scripts/normalize-character-images.py
+++ scripts/normalize-character-images.py
@@ -6,6 +6,21 @@ TARGET_RATIO = 1, 1
 directories = (Path('public/images/npcs'), Path('public/images/enemies'))
 paths = [path for directory in directories for path in sorted(directory.glob('*.png'))]
 
+
+def crop_height(image: Image.Image, target_height: int) -> Image.Image:
+    while image.height > target_height:
+        alpha = image.getchannel('A')
+        top = alpha.crop((0, 0, image.width, 1)).getbbox() is None
+        bottom = alpha.crop((0, image.height - 1, image.width, image.height)).getbbox() is None
+        if top:
+            image = image.crop((0, 1, image.width, image.height))
+        elif bottom:
+            image = image.crop((0, 0, image.width, image.height - 1))
+        else:
+            image = image.crop((0, 0, image.width, image.height - 1))
+    return image
+
+
 for path in paths:
     image = Image.open(path)
     alpha = image.getchannel('A') if 'A' in image.getbands() else None
@@ -18,7 +33,10 @@ for path in paths:
     width, height = image.size
     target_width = min(width, round(height * TARGET_RATIO[0] / TARGET_RATIO[1]))
     target_height = min(height, round(width * TARGET_RATIO[1] / TARGET_RATIO[0]))
-    left = (width - target_width) // 2
-    top = (height - target_height) // 2
-    image.crop((left, top, left + target_width, top + target_height)).save(path, format='PNG', optimize=True)
+    if height > target_height:
+        image = crop_height(image, target_height)
+    elif width > target_width:
+        left = (width - target_width) // 2
+        image = image.crop((left, 0, left + target_width, height))
+    image.save(path, format='PNG', optimize=True)
     print(path)
